a2b556c4c1333353278a8f96395a10635957ef08
Antonio Torres <megashadowmaster@yahoo.com>
2013-01-30 21:37:23 -0800

    updated php exercise
    from class 3

--- lesson-02/php-exercise.php
+++ lesson-02/php-exercise.php
@@ -4,27 +4,39 @@
 <meta charset="utf-8">
 <title>PHP Exercise</title>
 </head>
-
 <body>
 
 <?php
-echo age('1990-05-06');
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+?>
 
-function age($birthdate) {
-	return (strtotime('now') - strtotime($birthdate))/(60*60*24*365);
-}
+<?php
+	function age($birthdate) {
+		return (strtotime('now') - strtotime($birthdate))/(60*60*24*365);
+	}
 
-$age = age('1990-05-06');
+	$age = age($_POST['birthdate']);
 
-if($age > 21) {
-	?>
-    <h2 style="color: green">You're 21, yay!</h2>
-    <?php
-} else {
-    ?>
-    <h2 style="color: red">Nope you can't drink</h2>
-    <?php
-}
+	if($age > 21) {
+?>
+    	<h2 style="color: green">You're <?php echo intval($age) ?>. Yay! You can drink.</h2>
+        <img src="" width="100" height="100" />
+<?php
+	} else {
+?>
+    	<h2 style="color: red">Too young. You're <?php echo intval($age) ?>, you can't drink</h2>
+        <img src="" width="100" height="100" />
+<?php
+	}
+	} else {
+?>
+		<h3>Please Enter Your Birthdate:</h3>
+        <form action="" method="post">
+        <input name="birthdate" type="text">
+        <input name="" type="submit">
+        </form>
+<?php
+	}
 ?>
 
 </body>
